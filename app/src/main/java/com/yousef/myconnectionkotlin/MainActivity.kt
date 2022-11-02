a6e39fadb6766d88eb32eb2f127c2c53520e74bc
Yousef <yousefshaabansaad42@gmail.com>
2022-11-02 18:05:07 +0200

    c

--- app/src/main/java/com/yousef/myconnectionkotlin/MainActivity.kt
+++ app/src/main/java/com/yousef/myconnectionkotlin/MainActivity.kt
@@ -5,6 +5,15 @@ import android.os.Bundle
 import android.widget.Toast
 import com.yousef.internetconnectionkotlin.MyConnectionKt
 
+/**
+ * اللَّهُمَّ صَلِّ علَى مُحَمَّدٍ وعلَى آلِ مُحَمَّدٍ، كما صَلَّيْتَ علَى إبْرَاهِيمَ وعلَى آلِ إبْرَاهِيمَ؛ إنَّكَ حَمِيدٌ مَجِيدٌ
+ * اللَّهُمَّ بَارِكْ علَى مُحَمَّدٍ وعلَى آلِ مُحَمَّدٍ، كما بَارَكْتَ علَى إبْرَاهِيمَ وعلَى آلِ إبْرَاهِيمَ؛ إنَّكَ حَمِيدٌ مَجِيدٌ
+
+ *تم برمجتُه بواسطة : يوسف شعبان
+ * +201142747343
+ * yousefshaabansaad42@gmail.com
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,12 +22,12 @@ class MainActivity : AppCompatActivity() {
         val myConnectionKt = MyConnectionKt(this)
         if(myConnectionKt.isConnect()){
             // internet connection
-            Toast.makeText(this, "internet connection", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "internet connection", Toast.LENGTH_SHORT).show()
         }
 
         if(myConnectionKt.isDisconnect()) {
             // internet disconnect
-            myConnectionKt.showDisconnectDialog();
+            myConnectionKt.showDisconnectDialog()
         }
     }
 }
